3e7d0621940f178a8e2c34697c4d9f2a5e1dfd21
Antriel <Antriel@users.noreply.github.com>
2019-12-20 09:54:04 +0100

    basic chrome launcher

---
+++ src/whet/stones/ChromeStone.hx
@@ -0,0 +1,52 @@
+package whet.stones;
+
+class ChromeStone extends Whetstone {
+
+    var config:ChromeConfig;
+
+    public function new(project:WhetProject, config:ChromeConfig = null) {
+        super(project);
+        this.config = config == null ? { } : config;
+    }
+
+    #if !macro
+    @command public function launchChrome(opts:String) {
+        var args = [
+            '--app=http://localhost:7000/${config.launchPath}',
+            '--user-data-dir=${config.userDataDir}',
+        ];
+        if (config.remoteDebuggingPort != null) args.push('--remote-debugging-port=${config.remoteDebuggingPort}');
+        if (config.startFullScreen) args.push('--start-fullscreen');
+        if (config.startMaximized) args.push('--start-maximized');
+        for (flag in config.additionalFlags) args.push(flag);
+        #if hxnodejs
+        js.node.ChildProcess.spawn(config.chromePath, args, {
+            stdio: 'ignore',
+            detached: true
+        }).unref();
+        #elseif sys
+        new sys.io.Process(config.chromePath, args, true);
+        #else
+        Whet.error("Not implemented.");
+        #end
+    }
+    #end
+
+}
+
+@:structInit class ChromeConfig {
+
+    public var chromePath:String = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'; // TODO make more crossplatform
+    public var launchPath:String = "";
+    public var remoteDebuggingPort:Null<Int> = 9222;
+    public var additionalFlags:Array<String> = [];
+
+    /** Specifies if the browser should start in fullscreen mode, like if the user had pressed F11 right after startup. */
+    public var startFullScreen:Bool = false;
+
+    /** Starts the browser maximized, regardless of any previous settings. ↪ */
+    public var startMaximized:Bool = false;
+
+    public var userDataDir:String = '/tmp/chrome-debug';
+
+}
