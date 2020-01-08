15943005ac469f047719092274a2dbc0b28451c7
Antriel <Antriel@users.noreply.github.com>
2020-01-08 13:05:47 +0100

    fixed project commands context binding for JS

--- src/whet/WhetProject.hx
+++ src/whet/WhetProject.hx
@@ -16,7 +16,8 @@ class WhetProject {
         var fields = Type.getInstanceFields(Type.getClass(this));
         for (origField in Type.getInstanceFields(WhetProject)) fields.remove(origField);
         fields = fields.filter(f -> Reflect.isFunction(Reflect.field(this, f)));
-        for (f in fields) commands.set(f, Reflect.field(this, f));
+        for (f in fields) commands.set(f, function(arg)
+            Reflect.callMethod(this, Reflect.field(this, f), [arg])); // TODO deduplicate with whetstone?
     }
 
 }
