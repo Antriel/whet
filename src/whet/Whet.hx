682737313039d34cf6fe23d1091023cd9cc960a5
Antriel <Antriel@users.noreply.github.com>
2021-03-29 10:33:52 +0200

    allow specifying project, and using different project names

--- src/whet/Whet.hx
+++ src/whet/Whet.hx
@@ -3,6 +3,7 @@ package whet;
 import haxe.macro.Compiler;
 import haxe.macro.Context;
 import haxe.macro.Expr;
+import sys.FileSystem;
 
 class Whet {
 
@@ -17,20 +18,31 @@ class Whet {
         // In the future we should maybe use tink_cli for the commands and stuff, and handle running node
         // with the same commands instead of this mess.
         commands = [for (key => value in Context.getDefines())
-            if (key.indexOf('whet.') == 0 && key != 'whet.config') {
+            if (key.indexOf('whet.') == 0 && key != 'whet.project') {
                 command: key.substr('whet.'.length),
                 argument: value
             }
         ];
         var funcs = [];
-        for (t in Context.getModule('Project')) switch t {
+        var project = Context.definedValue('whet.project');
+        if (project == null) {
+            var projects = FileSystem.readDirectory('.')
+                .filter(s -> StringTools.endsWith(s, '.hx') && StringTools.contains(s, 'Project'))
+                .map(s -> StringTools.replace(s, '.hx', ''));
+            switch projects.length {
+                case 0: Context.error('No project to run found.', Context.currentPos());
+                case 1: project = projects[0];
+                case _: Context.error('Multiple projects found, specify one using `-D whet.project=Project`.', Context.currentPos());
+            }
+        }
+        for (t in Context.getModule(project)) switch t {
             case TInst(_.get() => { kind: KModuleFields(module), statics: _.get() => fields }, _):
                 for (f in fields) if (f.kind.match(FMethod(_))) {
                     var i = Lambda.findIndex(commands, c -> c.command.indexOf(f.name + "(") == 0);
                     if (i >= 0) {
                         var f = commands.splice(i, 1)[0].command;
                         f = StringTools.replace(f, ';', ',');
-                        funcs.push(Context.parse('Project.' + f, Context.currentPos()));
+                        funcs.push(Context.parse('$project.$f', Context.currentPos()));
                     }
                 }
             case _:
@@ -55,7 +67,6 @@ class Whet {
 
     @:noCompletion
     public static function executeProjects(commands:Array<{command:String, argument:String}>):Void {
-        // project.postInitTrigger.trigger(Noise);
         if (commands.length == 0) {
             msg('No command found. Use `-D whet.<command>=[arg]`.\nAvailable commands:');
             for (project in WhetProject.projects) for (meta in project.commandsMeta) {
