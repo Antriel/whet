7cc41ceab12cb0da2e78473ae0c01bd19aa38e34
Antriel <Antriel@users.noreply.github.com>
2022-04-21 12:18:01 +0200

    add shebang, package.json fixes

--- src/whet/Macros.hx
+++ src/whet/Macros.hx
@@ -3,10 +3,28 @@ package whet;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 
+using StringTools;
+using sys.io.File;
+
 class Macros {
 
     public static macro function getVersion():Expr {
         return macro $v{haxe.Json.parse(sys.io.File.getContent('package.json')).version};
+    }
+
+    public static macro function postprocess() {
+        var prefix = '#!/usr/bin/env node\n';
+        Context.onAfterGenerate(function() {
+            var file = haxe.macro.Compiler.getOutput();
+
+            switch file.getContent() {
+                case _.startsWith(prefix) => true:
+                case v:
+                    file.saveContent(prefix + v);
+            }
+
+        });
+        return null;
     }
 
 }
