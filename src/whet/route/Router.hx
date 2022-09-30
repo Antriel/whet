23c0f8c1b8ca2977cc157f162b97aae3a0172a10
Antriel <Antriel@users.noreply.github.com>
2022-09-30 15:52:58 +0200

    refactor Router, improve performance
    instead of getting the full list and then filtering it, filter paths as we walk them, and early exit if possible

--- src/whet/route/Router.hx
+++ src/whet/route/Router.hx
@@ -23,19 +23,41 @@ class Router {
      * @param pattern A glob pattern to search for.
      */
     public function get(pattern:MinimatchType = '**'):Promise<Array<RouteResult>> {
-        var filter = makeMinimatch(pattern);
+        return getResults(new Filters(makeMinimatch(pattern)), []);
+    }
+
+    function getResults(mainFilters:Filters, results:Array<RouteResult>):Promise<Array<RouteResult>> {
         return new Promise((res, rej) -> {
-            var result:Array<RouteResult> = [];
-            Promise.all([for (route in routes) {
-                allFromRoute(route).then(list -> {
-                    var routeUnderIsDir = route.routeUnder.isDir();
-                    for (item in list) {
-                        item.serveId = if (routeUnderIsDir) item.serveId.getPutInDir(route.routeUnder);
-                        else route.routeUnder;
-                        if (filter.match(item.serveId)) result.push(item);
-                    }
-                });
-            }]).then(_ -> res(result));
+            var allRouteProms = [];
+            for (route in routes) {
+                var routeFilters = mainFilters.clone(); // Clone filters so other routes aren't affected.
+                var possible = if (route.routeUnder.isDir()) routeFilters.step(route.routeUnder);
+                else routeFilters.finalize(route.routeUnder);
+
+                if (!possible) continue;
+                if (route.filter != null || route.extractDirs != null) routeFilters.add(route.filter, route.extractDirs);
+
+                var prom:Promise<Any> = if (route.source is AnyStone) {
+                    final stone:AnyStone = cast route.source;
+                    stone.list().then(list -> {
+                        for (sourceId in list) {
+                            // We are finished for this route, check the last filter if any.
+                            var finalFilters = routeFilters.clone();
+                            var passed = finalFilters.finalize(sourceId);
+                            // This could be optimized. We don't need to do checks, unless we have a new filter.
+                            // And we should be able to avoid cloning regardless, by avoiding making modifications to the Filters.
+                            if (passed) {
+                                var serveId = finalFilters.getServeId();
+                                results.push({ source: stone, sourceId: sourceId, serveId: serveId });
+                            }
+                        }
+                    });
+                } else if (route.source is Router) {
+                    (cast route.source:Router).getResults(routeFilters, results);
+                } else throw new js.lib.Error("Router source must be a Stone or a Router.");
+                allRouteProms.push(prom);
+            }
+            Promise.all(allRouteProms).then(_ -> res(results));
         });
     }
 
@@ -73,50 +95,96 @@ class Router {
         });
     }
 
-    function allFromRoute(route:RoutePath):Promise<Array<RouteResult>> {
-        if (route.source is AnyStone) {
-            final stone:AnyStone = cast route.source;
-            return stone.list().then(list -> {
-                var arr:Array<RouteResult> = [];
-                for (path in list) {
-                    var serveId = getServeId(path, route);
-                    if (serveId != null)
-                        arr.push({ source: stone, sourceId: path, serveId: serveId });
-                }
-                return arr;
-            });
-        } else if (route.source is Router) {
-            final router:Router = cast route.source;
-            return router.get().then(list -> {
-                var arr:Array<RouteResult> = [];
-                for (result in list) {
-                    var serveId = getServeId(result.serveId, route);
-                    if (serveId != null) {
-                        result.serveId = serveId;
-                        arr.push(result);
-                    }
-                }
-                return arr;
-            });
-        } else throw new js.lib.Error("Router source must be a Stone or a Router.");
+}
+
+typedef Filter = {pathSoFar:Array<String>, filter:Minimatch, inProgress:Bool, remDirs:Array<Minimatch>};
+
+abstract Filters(Array<Filter>) from Array<Filter> {
+
+    public inline function new(filter:Minimatch) { // TODO allow null minimatch instead of "**", and just ignore the checks.
+        this = [{ pathSoFar: [], filter: filter, inProgress: true, remDirs: [] }];
     }
 
-    inline function getServeId(path:SourceId, route:RoutePath) {
-        var serveId = null;
-        if (route.filter == null || route.filter.match(path)) {
-            serveId = path;
-            if (route.extractDirs != null) {
-                var dir:String = path;
-                do {
-                    dir = dir.substring(0, dir.lastIndexOf('/'));
-                    if (route.extractDirs.match(dir + '/')) {
-                        serveId = path.getRelativeTo(dir + '/');
-                        break;
-                    }
-                } while (dir.length > 0);
+    public inline function finalize(finalPath:SourceId):Bool {
+        var parts = finalPath.split('/');
+        return iterate(f -> {
+            f.inProgress = false;
+            processParts(parts, f);
+            f.pathSoFar = f.pathSoFar.concat(parts);
+            return f.filter == null || f.filter.match(Path.posix.join(...f.pathSoFar));
+        });
+    }
+
+    public inline function step(dir:SourceId):Bool {
+        if (dir.length == 0 || dir == './') return true;
+        var parts = dir.split('/').filter(p -> p.length > 0);
+        return iterate(f -> {
+            processParts(parts, f);
+            f.pathSoFar = f.pathSoFar.concat(parts);
+            return f.filter == null || isPathPossible(f);
+        });
+    }
+
+    inline function iterate(action:Filter->Bool) {
+        var res = true;
+        var i = this.length;
+        while (--i >= 0) { // Iterate in reverse, as we modify `parts` as we go.
+            var f = this[i];
+            if (f.inProgress) if (!action(f)) {
+                res = false;
+                break;
             }
         }
-        return serveId;
+        return res;
+    }
+
+    public inline function getServeId():SourceId return Path.posix.join(...this[0].pathSoFar);
+
+    public inline function add(f:Minimatch, extractDir:Minimatch):Void {
+        if (extractDir != null && this.length > 0) // Add to the current one. Applied when adding paths.
+            this[this.length - 1].remDirs.push(extractDir);
+        this.push({
+            pathSoFar: [],
+            filter: f,
+            inProgress: true,
+            remDirs: []
+        });
+    }
+
+    public inline function clone():Filters {
+        return this.map(f -> {
+            filter: f.filter,
+            pathSoFar: f.pathSoFar.copy(),
+            inProgress: f.inProgress,
+            remDirs: f.remDirs.copy()
+        });
+    }
+
+    inline function processParts(parts:Array<String>, f:Filter) {
+        if (f.remDirs.length == 0) return;
+        var i = parts.length;
+        while (--i >= 0) {
+            for (r in f.remDirs) {
+                if (r.match(parts[i] + '/')) {
+                    parts.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+
+    function isPathPossible(f:Filter):Bool {
+        inline function onHit(hit:Bool) {
+            if (f.filter.options.flipNegate) return hit;
+            return hit != f.filter.negate;
+        }
+        // If we are only interested in base filename, we can't determine result yet.
+        if (f.filter.options.matchBase) return true;
+        for (set in f.filter.set) {
+            final hit = f.filter.matchOne(cast f.pathSoFar, set, true); // (`cast` needed – wrong type definitions.)
+            if (hit) return onHit(true);
+        }
+        return onHit(false); // No hit.
     }
 
 }
