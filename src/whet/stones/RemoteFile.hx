0d7c03366b73f85a8ea1832d8edbc4afaf09333f
Antriel <Antriel@users.noreply.github.com>
2022-10-19 12:20:35 +0200

    RemoteFile handle redirects

--- src/whet/stones/RemoteFile.hx
+++ src/whet/stones/RemoteFile.hx
@@ -16,22 +16,27 @@ class RemoteFile extends Stone<RemoteFileConfig> {
         #if tests
         var file = '' + TestRemoteFileStone.counter++;
         #else
-        return new Promise((res, rej) -> {
-            Https.get(config.url, response -> {
-                if (response.statusCode < 200 || response.statusCode >= 300) {
-                    // Must consume response data to free up memory.
-                    response.resume();
-                    throw new js.lib.Error('Error downloading file. ${response.statusCode} – ${response.statusMessage}');
-                }
-                final bufs = [];
-                response.on('data', d -> bufs.push(d));
-                response.on('end', function() {
-                    final data = Buffer.concat(bufs);
-                    res([SourceData.fromBytes(getId(), data)]);
-                });
+        return new Promise((res, rej) -> get(config.url, res, rej));
+        #end
+    }
+
+    function get(url:String, res, rej) {
+        Https.get(url, response -> {
+            if (response.statusCode == 301 || response.statusCode == 302) {
+                return get(response.headers.get('location'), res, rej);
+            }
+            if (response.statusCode < 200 || response.statusCode >= 300) {
+                // Must consume response data to free up memory.
+                response.resume();
+                throw new js.lib.Error('Error downloading file. ${response.statusCode} – ${response.statusMessage}');
+            }
+            final bufs = [];
+            response.on('data', d -> bufs.push(d));
+            response.on('end', function() {
+                final data = Buffer.concat(bufs);
+                res([SourceData.fromBytes(getId(), data)]);
             });
         });
-        #end
     }
 
     override function list():Promise<Array<SourceId>> return Promise.resolve([getId()]);
